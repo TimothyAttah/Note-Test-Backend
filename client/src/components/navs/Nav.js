0e913c57752637542fc08d4c90c008946e94c588
Timothy Attah <timothyattah733@gmail.com>
2021-04-20 20:18:54 +0100

    Created helper.js and nav.js

---
+++ client/src/components/navs/Nav.js
@@ -0,0 +1,18 @@
+import { IconButton } from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <IconButton>
+       <Menu />
+      </IconButton>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Nav
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Nav from '../../components/navs/Nav'
 
 const Home = () => {
   return (
     <div>
+      <Nav />
       <h1>Home Page</h1>
     </div>
   )
