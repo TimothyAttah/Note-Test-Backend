04a06f299ec43d68665b9960464de6bcdf93b9e0
Timothy Attah <timothyattah733@gmail.com>
2021-04-20 18:18:32 +0100

    Added header styles in header.js

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { MenuBook } from '@material-ui/icons'
-import styled from 'styled-components';
+import { MenuBook } from '@material-ui/icons';
+
 import Buttons from '../buttons/Button';
-import { colors } from '../elements';
-
-const Headers = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 15px 20px;
-  position: sticky;
-  top: 0;
-  background-color: white;
-  box-shadow: 0px 5px 8px -9px rgba(0, 0, 0, 0.75);
-  h1 > a {
-    color: ${colors.teal};
-    font-family: 'Cursive', serif;
-  }
-`;
-
+import { Headers } from './HeaderStyles';
 
 const Header = () => {
   return (
     <Headers>
-      <h1><Link>Note3Sixty<MenuBook /></Link></h1>
+      <h1><Link to='/api/user'>Note3Sixty<MenuBook /></Link></h1>
+      <h2><Link to='/api/user'><MenuBook /></Link></h2>
+
       <div>
         <Buttons
           first='Signup'
---
+++ client/src/components/header/HeaderStyles.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import { colors } from '../elements';
+
+export const Headers = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 20px;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  box-shadow: 0px 5px 8px -9px rgba(0, 0, 0, 0.75);
+  h1 > a {
+    color: ${colors.teal};
+    font-family: 'Cursive', serif;
+  }
+    h2 {
+      display: none;
+    }
+  @media (max-width: 414px){
+    h1 {
+      display: none;
+    }
+    h2 {
+     display: block;
+     a {
+      color: ${colors.teal};
+     }
+    }
+    .MuiButtonGroup-groupedContainedPrimary:not(:last-child),
+    .MuiButtonGroup-groupedHorizontal:not(:first-child){
+       font-size: 12px;
+       padding: 5px;
+    }
+  }
+  @media (max-width: 375px){
+     .MuiButtonGroup-groupedContainedPrimary:not(:last-child),
+    .MuiButtonGroup-groupedHorizontal:not(:first-child){
+       font-size: 8px;
+       padding: 5px;
+       font-weight: bold;
+       letter-spacing: 0.8px;
+    }
+  }
+`;
