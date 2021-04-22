c27796ec24c37c447deee158a844698717ec03a3
Timothy Attah <timothyattah733@gmail.com>
2021-04-22 20:50:47 +0100

    Created notes model

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require( 'dotenv' )
 dotenv.config({path: './config/key'})
 
 require( './models/UserModel' );
+require( './models/NoteModel' );
 
 const app = express();
 
@@ -13,23 +14,11 @@ connectDB();
 app.use( express.json() );
 app.use( cors() );
 
-// app.get( '/', ( req, res ) => {
-//   res.send( 'Hello mighty worlds' )
-// } );
-
 app.use( '/users', require( './routes/authRoutes' ) );
 
 
 const PORT = process.env.PORT || 8080;
 
-// if ( process.env.NODE_ENV === 'production' ) {
-//   app.use( express.static( path.join( __dirname, 'client/build' ) ) );
-  
-//   app.get( '*', ( req, res ) => {
-//     // res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html'))
-//     res.sendFile(path.join(__dirname, '/client/build/index.html', 'index.html'));
-//   })
-// }
 
 if ( process.env.NODE_ENV === 'production' ) {
   app.use( express.static( 'client/build' ) );
