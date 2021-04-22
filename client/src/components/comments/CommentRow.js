2304a4e64876007388dcf9e2d175a3449254447c
Timothy Attah <timothyattah733@gmail.com>
2021-04-22 17:09:48 +0100

    Deleted IconButton

--- client/src/components/comments/CommentRow.js
+++ client/src/components/comments/CommentRow.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Avatar, IconButton } from '@material-ui/core';
-import { Delete, MoreHoriz, ThumbUp } from '@material-ui/icons';
+import { Avatar } from '@material-ui/core';
+import { Delete, ThumbUp } from '@material-ui/icons';
 import styled, { css } from 'styled-components';
 
 const Comments = styled.div`
