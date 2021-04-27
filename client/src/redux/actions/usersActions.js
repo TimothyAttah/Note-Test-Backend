a349cdd15d45a46e82e6fe068d1959acb8e98bd2
Timothy Attah <timothyattah733@gmail.com>
2021-04-27 17:14:56 +0100

    Created follow users actions

--- client/src/redux/actions/usersActions.js
+++ client/src/redux/actions/usersActions.js
@@ -1,4 +1,4 @@
-import { GET_USER } from '../type';
+import { FOLLOW_USER, GET_USER } from '../type';
 import * as api from '../api/notesApi';
 
 export const getUser = (id) => async dispatch => {
@@ -11,4 +11,29 @@ export const getUser = (id) => async dispatch => {
   } catch (error) {
     console.log(error);
   }
+}
+
+
+export const followUsers = (followId) => dispatch => {
+  fetch( 'http://localhost:8080/auth/users/follow', {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer "+localStorage.getItem('jwt')
+    },
+    body: JSON.stringify({followId})
+  } ).then( res => res.json() )
+    .then( data => {
+      if ( data.error ) {
+        console.log( data.error );
+      } else {
+        console.log( data.message );
+        dispatch( {
+          type: FOLLOW_USER,
+          payload: data.result
+        } )
+    }
+    } ).catch( err => {
+    console.log(err);
+  })
 }
--- client/src/redux/type.js
+++ client/src/redux/type.js
@@ -3,6 +3,8 @@ export const SIGNUP_USER = 'SIGNUP_USER';
 export const GET_USERS = 'GET_USERS';
 export const GET_USER = 'GET_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
+export const FOLLOW_USER = 'FOLLOW_USER';
+export const UNFOLLOW_USER = 'UNFOLLOW_USER';
 
 export const NOTES_CREATE = 'NOTES_CREATE'
 export const NOTES_LISTS = 'NOTES_LISTS'
