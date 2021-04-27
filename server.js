f74b96d7c1d0b0b6e65c2f088be46b5aa5548b82
Timothy Attah <timothyattah733@gmail.com>
2021-04-27 13:22:49 +0100

    Created UserAuth routes

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use( cors() );
 
 app.use( '/users', require( './routes/authRoutes' ) );
 app.use( '/notes', require( './routes/notesRoutes' ) );
+app.use('/auth', require('./routes/userRoutes'))
 
 
 const PORT = process.env.PORT || 8080;
