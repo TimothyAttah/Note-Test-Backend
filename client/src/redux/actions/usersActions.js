0d78c4e9d8455b95dab27b4ae73c615cfeec7e2d
Timothy Attah <timothyattah733@gmail.com>
2021-04-27 16:24:56 +0100

    Created followers and following schema in userModel.js

--- client/src/redux/actions/usersActions.js
+++ client/src/redux/actions/usersActions.js
@@ -4,12 +4,11 @@ import * as api from '../api/notesApi';
 export const getUser = (id) => async dispatch => {
   try {
     const { data } = await api.getUser( id );
-    console.log( data );
     dispatch( {
       type: GET_USER,
       payload: data
     })
   } catch (error) {
-    
+    console.log(error);
   }
 }
