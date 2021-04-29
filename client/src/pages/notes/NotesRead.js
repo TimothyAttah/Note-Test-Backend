13e6c63fde553cfeb77ecc0551805cff0971e103
Timothy Attah <timothyattah733@gmail.com>
2021-04-29 02:42:39 +0100

    Added media queries to Notesread.js file

--- client/src/pages/notes/NotesRead.js
+++ client/src/pages/notes/NotesRead.js
@@ -10,18 +10,26 @@ import { user } from '../../components/NameInitials';
 const Note = styled.div`
   display: flex;
   justify-content: space-between;
+  @media (max-width: 600px){
+    display: block;
+  }
 `;
 
 const NoteLeft = styled.div`
-  width: 300px;
   ${props => props.primary && css`
   border: 2px solid black;
-    display: flex;
-    justify-content: space-around;
-    padding: 10px 0;
+    display: block; 
+    padding: 10px 20px;
   .MuiAvatar-root {
     width: 120px;
     height: 120px;
+  }
+  @media (max-width: 600px){
+    padding-left: 100px;
+    border: none;
+  }
+  @media (max-width: 375px){
+    padding-left: 50px;
   }
   `}
   ${props => props.secondary && css`
@@ -38,11 +46,52 @@ const NoteLeft = styled.div`
        display: flex;
         align-items: center;
     }
+    @media (max-width: 600px){
+    padding-left: 100px;
+    width: 340px;
+  }
+  @media (max-width: 375px){
+    padding-left: 50px;
+    
+  }
+  @media (max-width: 375px){
+    padding-left: 50px;
+
+  }
+  @media (max-width: 320px){
+   
+    width: 300px;
+  }
+  @media (max-width: 280px){
+   
+    width: 260px;
+    font-size: 12px;
+  }
   `}
+  ${props => props.button && css`
+    padding-bottom: 30px;
+    @media (max-width: 600px){
+    padding-left: 100px;
+    padding-bottom: 80px;
+  }
+  @media (max-width: 375px){
+    padding-left: 50px;
+  }
+  `}
+  @media (max-width: 600px){
+   h1 {
+     padding-left: 100px;
+   }
+   @media (max-width: 375px){
+    h1 {
+      padding-left: 50px;
+    }
+  }
+  }
 `;
 
 const NoteRight = styled.div`
-  width: 800px;
+  width: 70%;
   border: 2px solid #ddd;
   h2 {
     padding-top: 50px;
@@ -50,9 +99,18 @@ const NoteRight = styled.div`
     
   }
   p {
-    width: 600px;
-    padding: 20px;
+    width: 100%;
+    padding: 20px 50px 20px 20px;
     line-height: 25px;
+  }
+  @media (max-width: 900px){
+    width: 55%;
+  }
+  @media (max-width: 600px){
+    width: 100%;
+    h2 {
+      padding-top: 10px;
+    }
   }
 `;
 
@@ -73,7 +131,7 @@ const NotesRead = () => {
       {note ? (
         <Note>
           <NoteLeft>
-            <h2>Posted By: <br /></h2>
+            <h1>Posted By:</h1>
             <NoteLeft primary>
               <div> <Avatar></Avatar></div>
               <div>
@@ -88,11 +146,11 @@ const NotesRead = () => {
               <div>{note.postedBy.following.length } <h4>Following</h4></div>
               <div>{note.likes.length} <h4><ThumbUp /></h4></div>
             </NoteLeft>
-            <div>
+            <NoteLeft button>
               <Link to={ note && note.postedBy._id !== user.results._id ? `/api/auth/users/${ note.postedBy._id }/user/profile` : `/api/users/profile` }>
                 <Button variant='contained'>More from ...</Button>
               </Link>
-            </div>
+            </NoteLeft>
           </NoteLeft>
           <NoteRight>
             <h2>{ note.title }</h2>
