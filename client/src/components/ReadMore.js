78ade277da21884eb59b95d4671501ac65b60384
Timothy Attah <timothyattah733@gmail.com>
2021-04-29 07:29:50 +0100

    Added styles to profile.js file

--- client/src/components/ReadMore.js
+++ client/src/components/ReadMore.js
@@ -1,16 +1,23 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const ReadMoreWrapper = styled.div`
+  p {
+    padding: 20px 0;
+    line-height: 22px;
+  }
+`;
+
 
 const ReadMore = ( { children, maxCharacterCount = 150 } ) => {
   const [ isTruncated ] = useState( true );
-
   const text = children
-
-  const resultString = isTruncated ? text.slice(0, maxCharacterCount) : text
+  const resultString = isTruncated ? text.slice( 0, maxCharacterCount ) : text
   return (
-    <div>
-      {resultString}...
-    </div>
-  )
-}
+    <ReadMoreWrapper>
+      <p>{resultString }...</p>
+    </ReadMoreWrapper>
+  );
+};
 
-export default ReadMore
+export default ReadMore;
