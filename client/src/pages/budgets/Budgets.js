de37f4ff05946e73dfe1f59788ec3b35db1fef9f
Timothy Attah <timothyattah733@gmail.com>
2021-05-01 17:15:56 +0100

    Dispatched listIncome actions to IncomeBudgets.js

--- client/src/pages/budgets/Budgets.js
+++ client/src/pages/budgets/Budgets.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Nav from '../../components/navs/Nav';
+import IncomeBudgets from './IncomeBudgets';
 
 const Budgets = () => {
   return (
     <div>
       <Nav />
       <h1>Budgets Page</h1>
+      <div>
+        <IncomeBudgets />
+      </div>
     </div>
   )
 }
--- client/src/pages/budgets/IncomeBudgets.js
+++ client/src/pages/budgets/IncomeBudgets.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { listIncomes} from '../../redux/actions/incomesAction';
+
 
 const IncomeBudgets = () => {
+  const dispatch = useDispatch();
+  useEffect( () => {
+    dispatch( listIncomes() )
+  }, [ dispatch ] );
+
+  const incomes = useSelector( state => state.incomesReducer.incomes );
+  console.log(incomes);
   return (
     <div>
-      <h1>Income Budgets Page</h1>
+      <h1>Income Transaction</h1>
+      {incomes ? (
+        incomes.map( income => {
+          return (
+            <ul key={income.id}>
+              <li>{ income.item } <span>{ income.value }</span></li>
+            </ul>
+          )
+        })
+      ): (
+        <h2>Loading...</h2>
+      )}
     </div>
   )
 }
