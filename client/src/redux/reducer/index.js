5c9635199f07f611b64db5cec90806aa4aa37533
Timothy Attah <timothyattah733@gmail.com>
2021-05-01 17:06:27 +0100

    Created incomes actions

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -3,10 +3,12 @@ import auth from './authReducer'
 import notesReducer from './notesReducer';
 import todosReducer from './todosReducer';
 import usersReducer from './usersReducer';
+import incomesReducer from './incomeReducer';
 
 export default combineReducers( {
   auth,
   notesReducer,
   todosReducer,
-  usersReducer
+  usersReducer,
+  incomesReducer
 } );
