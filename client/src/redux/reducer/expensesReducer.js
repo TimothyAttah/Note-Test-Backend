c21f2372ebd3dd94000386cf3c0a5b15ae9b0ce3
Timothy Attah <timothyattah733@gmail.com>
2021-05-01 18:03:20 +0100

    Created expenses reducer

---
+++ client/src/redux/reducer/expensesReducer.js
@@ -0,0 +1,39 @@
+import { v4 } from 'uuid';
+import { CREATE_EXPENSES, DELETE_EXPENSES, EDIT_EXPENSES, LIST_EXPENSES } from '../type';
+
+const initialState = {
+  expenses: [
+    {id: v4(), item: 'Groceries', value: 2000},
+    {id: v4(), item: 'house rent', value: 5000},
+    {id: v4(), item: 'brought car', value: 1500},
+  ]
+}
+
+const ExpensesReducer = ( state = initialState, action ) => {
+  switch ( action.type ) {
+    case LIST_EXPENSES:
+      return {
+        ...state
+      }
+    case CREATE_EXPENSES:
+      return {
+        ...state,
+        expenses: [ action.payload, ...state.expenses ]
+      };
+    case DELETE_EXPENSES:
+      return {
+        ...state,
+        expenses: state.expenses.filter( expense=> expense.id !== action.payload )
+      };
+    case EDIT_EXPENSES:
+      return {
+        ...state,
+        expenses: state.expenses.map(expense=> expense.id === action.payload.id ? action.payload : expense)
+      }
+    default:
+      return state
+  }
+}
+
+export default ExpensesReducer;
+
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -4,11 +4,13 @@ import notesReducer from './notesReducer';
 import todosReducer from './todosReducer';
 import usersReducer from './usersReducer';
 import incomesReducer from './incomeReducer';
+import expensesReducer from './expensesReducer';
 
 export default combineReducers( {
   auth,
   notesReducer,
   todosReducer,
   usersReducer,
-  incomesReducer
+  incomesReducer,
+  expensesReducer,
 } );
