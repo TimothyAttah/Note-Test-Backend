98245eb890f6f3a42dcaf562688d95ef737ce68e
Timothy Attah <timothyattah733@gmail.com>
2021-05-01 21:10:26 +0100

    Created and dispatched createIncome and createExpenses actions to budgets form

--- client/src/components/forms/IncomeForm.js
+++ client/src/components/forms/IncomeForm.js
@@ -2,12 +2,15 @@ import React, {useState} from 'react';
 import { Button} from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { createIncome } from '../../redux/actions/incomesAction';
+import { createExpenses } from '../../redux/actions/expensesActions';
 import styled from 'styled-components';
 import BudgetsNav from '../navs/BudgetsNav';
 import { Add } from '@material-ui/icons';
+import e from 'cors';
+import { v4 } from 'uuid';
 
 
-const FormContainer = styled.form`
+const FormContainer = styled.div`
   border: 2px solid yellow;
   display: flex;
   align-items: center;
@@ -47,42 +50,76 @@ const FormBudgetButton = styled.div`
 
 const IncomeForm = () => {
   const [ isOpen, setIsOpen ] = useState( false )
+  const [ item, setItem ] = useState( '' )
+  const [ value, setValue ] = useState( '')
+  const dispatch = useDispatch();
+  
+  
 
+  const handleSubmit = ( e ) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: v4(),
+      item,
+      value
+    }
+  
+    isOpen ? (
+      dispatch( createExpenses( newTransaction ) )
+    ) : (
+      dispatch( createIncome( newTransaction ) )
+    )
+    
+  }
+
+   
  
   return (
-    <FormContainer>
+    <FormContainer >
       <FormBudgetButton>
         <h4>Choose Transaction </h4>
         <BudgetsNav setIsOpen={ setIsOpen } />
       </FormBudgetButton>
       { isOpen ? (
-        <IncomeType primary>
+        <IncomeType primary onSubmit={handleSubmit}>
           <label>Expenses Item:</label>
           <input
             type='text'
             placeholder='Enter expenses items...'
             className='input-text'
+            onChange={ (e)=> setItem(e.target.value) }
+            value={ item }
+            name='item'
           />
           <label>Expenses Value:</label>
           <input
             type='number'
+            onChange={ (e) => setValue(e.target.value)}
+            name='value'
+            value={value}
           />
-          <Button variant='contained' size='small'>-</Button>
+          <Button type='submit' variant='contained' size='small'>-</Button>
         </IncomeType>
       ) : (
-        <IncomeType>
+        <IncomeType onSubmit={handleSubmit}>
           <label>Income Item:</label>
           <input
             type='text'
             placeholder='Enter income items...'
-            className='input-text'
+              className='input-text'
+              onChange={ (e)=> setItem(e.target.value) }
+              name='item'
+              value={item}
           />
           <label>Income Value:</label>
           <input
             type='number'
-            className='input-value'
+              className='input-value'
+              onChange={ (e) => setValue(e.target.value)}
+              name='value'
+              value={value}
           />
-          <Button variant='contained' size='small' color='inherit'><Add /></Button>
+          <Button type='submit' variant='contained' size='small' color='inherit'><Add /></Button>
         </IncomeType>
       ) }
     </FormContainer>
