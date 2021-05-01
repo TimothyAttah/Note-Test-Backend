611463756212de543f86f5be805a5e9811d80783
Timothy Attah <timothyattah733@gmail.com>
2021-05-01 17:37:42 +0100

    Added styles to incomeBudgets.js

--- client/src/pages/budgets/IncomeBudgets.js
+++ client/src/pages/budgets/IncomeBudgets.js
@@ -1,6 +1,44 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { listIncomes} from '../../redux/actions/incomesAction';
+import { listIncomes } from '../../redux/actions/incomesAction';
+
+import styled from 'styled-components';
+import { Delete, Edit } from '@material-ui/icons';
+import { IconButton } from '@material-ui/core';
+
+const IncomesContainer = styled.div`
+  width: 400px;
+`;
+
+const Incomes = styled.ul`
+  padding: 0;
+  margin: 0;
+  li {
+    border: 2px solid green;
+    padding: 10px;
+    margin: 10px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const IncomesItems = styled.div`
+  display: flex;
+  align-items: center;
+  text-transform: capitalize;
+  font-weight: bold;
+  span {
+     padding-left: 10px;
+     color: gray;
+  }
+`;
+
+const IncomesButton = styled.div`
+  .MuiIconButton-root {
+    padding: 0;
+  }
+`;
 
 
 const IncomeBudgets = () => {
@@ -12,20 +50,26 @@ const IncomeBudgets = () => {
   const incomes = useSelector( state => state.incomesReducer.incomes );
   console.log(incomes);
   return (
-    <div>
+    <IncomesContainer>
       <h1>Income Transaction</h1>
       {incomes ? (
         incomes.map( income => {
           return (
-            <ul key={income.id}>
-              <li>{ income.item } <span>{ income.value }</span></li>
-            </ul>
+            <Incomes key={income.id}>
+              <li>
+                <IncomesItems>{ income.item }: <span>{ income.value }</span></IncomesItems>
+                <IncomesButton>
+                  <IconButton><Edit /></IconButton>
+                  <IconButton> <Delete /></IconButton>
+                </IncomesButton>
+              </li>
+            </Incomes>
           )
         })
       ): (
         <h2>Loading...</h2>
       )}
-    </div>
+    </IncomesContainer>
   )
 }
 
