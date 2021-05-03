9db20622a758e171445f1ca9961ff9866ed8d19a
Timothy Attah <timothyattah733@gmail.com>
2021-05-03 23:42:52 +0100

    Created Budgets Page

--- client/src/pages/budgets/BudgetsTransaction.js
+++ client/src/pages/budgets/BudgetsTransaction.js
@@ -6,7 +6,7 @@ import { listExpenses } from '../../redux/actions/expensesActions';
 import {images} from '../../components/Images'
 
 const BalanceContainer = styled.div`
-  /* width: 100%; */
+  width: 100%;
   height: 350px;
   background: url(${images.backgroundImgTwo});
   background-repeat: no-repeat;
@@ -16,181 +16,82 @@ const BalanceContainer = styled.div`
 `;
 
 const TotalBalance = styled.div`
+border: 2px solid blue;
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
   align-items: center;
+  flex-wrap: wrap;
+ width: 100%;
   color: #fff;
   font-size: 30px;
   position: relative;
   padding-top: 30px;
   padding-bottom: 50px;
-  padding-left: 300px;
   opacity: 0.9;
   overflow: hidden;
-  span {
-    position: absolute;
-    right: 300px;
-  };
-   @media (max-width:1125px){
-    padding-left: 220px;
-    span {
-    right: 200px;
-  };
+  h2 {
+   padding-right: 10px;
   }
-   @media (max-width:950px){
-    padding-left: 180px;
-    span {
-    right: 120px;
-  };
-  }
-   @media (max-width:850px){
-     font-size: 20px;
-    padding-left: 180px;
-    span {
-    right: 150px;
-  };
-  }
-   @media (max-width:700px){
-     font-size: 20px;
-    padding-left: 120px;
-    span {
-    right: 100px;
-  };
-  }
-   @media (max-width:575px){
-     font-size: 18px;
-    padding-left: 80px;
-    span {
-    right: 100px;
-  };
-  }
-   @media (max-width:500px){
-     font-size: 15px;
-    padding-left: 80px;
-    span {
-    right: 60px;
-  };
-  }
-   @media (max-width:415px){
-     font-size: 14px;
-    padding-left: 60px;
-    span {
-    right: 40px;
-  };
-  }
-   @media (max-width:360px){
-     font-size: 12px;
-    padding-left: 40px;
-    span {
-    right: 20px;
-  };
-  }
-   @media (max-width:280px){
-     font-size: 12px;
-    padding-left: 20px;
-    span {
-    right: 20px;
-  };
-  }
-  /* @media (max-width:1300px){
-   justify-content: flex-start;
-   padding-left: 180px;
-   span {
-     padding-right: 200px;
-   }
-  } */
-  /* @media (max-width:800px){
+  @media (max-width: 900px){
     font-size: 20px;
-   padding-left: 120px;
-   span {
-     padding-right: 350px;
-   }
-  } */
-  /* @media (max-width:768px){
-    font-size: 20px;
-   padding-left: 100px;
-   span {
-     padding-right: 350px;
-   }
   }
-  @media (max-width:650px){
-    font-size: 18px;
-   padding-left: 100px;
-   span {
-     padding-right: 480px;
-   }
+  @media (max-width: 550px){
+    font-size: 16px;
   }
-  @media (max-width:530px){
-    font-size: 15px;
-    font-weight: bold;
-   padding-left: 60px;
-   span {
-     padding-right: 550px;
-   }
-  }
-  @media (max-width:450px){
-   span {
-     padding-right: 630px;
-   }
-  }
-   @media (max-width:380px){
+  @media (max-width: 375px){
     font-size: 12px;
-    font-weight: bold;
-   padding-left: 40px;
-   span {
-     padding-right: 690px;
-   }
+    h1, h2{
+      font-size: 20px;
+    }
   }
-   @media (max-width:380px){
-   padding-left: 20px;
-   span {
-     padding-right: 730px;
-   }
-  } */
-`;
+  `;
 
 const TotalContainer = styled.div`
   padding-left: 450px;
   padding-top: 30px;
-  @media (max-width:1125px){
-    padding-left: 400px;
-  }
-  @media (max-width:950px){
+   @media (max-width: 1000px){
     padding-left: 300px;
   }
-  @media (max-width:850px){
-    padding-left: 250px;
-  }
-  @media (max-width:700px){
+   @media (max-width: 700px){
     padding-left: 200px;
   }
-  @media (max-width:575px){
-    padding-left: 120px;
+   @media (max-width: 650px){
+    padding-left: 150px;
   }
-  @media (max-width:415px){
-    padding-left: 80px;
-  }
-  @media (max-width:320px){
-    padding-left: 60px;
-  }
-  @media (max-width:290px){
-    padding-left: 35px;
-  }
-  /* @media (max-width: 1000px){
-    padding-left: 250px;
-  }
-  @media (max-width: 800px){
-    padding-left: 180px;
-  }
-  @media (max-width: 650px){
+   @media (max-width: 450px){
     padding-left: 100px;
   }
-  @media (max-width: 450px){
+   @media (max-width: 414px){
     padding-left: 60px;
   }
-  @media (max-width: 380px){
-    padding-left: 40px;
-  } */
+   @media (max-width: 375px){
+    padding-left: 50px;
+  }
+   @media (max-width: 320px){
+    padding-left: 20px;
+  }
+`;
+
+
+const BalanceWrapper = styled.div`
+  padding-left: 320px;
+  border: 2px solid wheat;
+ width: 80%;
+ @media (max-width: 1000px){
+    padding-left: 180px;
+  }
+    @media (max-width: 650px){
+    padding-left: 90px;
+  }
+    @media (max-width: 450px){
+    padding-left: 50px;
+  }
+    @media (max-width: 400px){
+    padding-left: 30px;
+  }
+    @media (max-width: 375px){
+    padding-left: 10px;
+  }
 `;
 
 const TotalIncome = styled.div`
@@ -205,30 +106,25 @@ const TotalIncome = styled.div`
     right: 20px;
     letter-spacing: 1.5px;
   };
-   @media (max-width:850px){
-    width: 300px;
+   @media (max-width: 900px){
+   font-size: 15px;
+   width: 300px;
   }
-   @media (max-width:575px){
-     font-size: 12px;
-      span {
-    letter-spacing: 1px;
-  };
+   @media (max-width: 550px){
+   font-size: 13px;
+   width: 220px;
+   span {
+     right: 10px;
+     letter-spacing: 1px;
+   }
   }
-   @media (max-width:500px){
-     width: 250px;
-     font-size: 10px;
-     font-weight: bolder;
-      span {
-    letter-spacing: 0.5px;
-  };
-  }
-   @media (max-width:375px){
-     width: 200px;
-     font-size: 11px;
-      span {
-    letter-spacing: 0.5px;
-    right: 10px;
-  };
+   @media (max-width: 375px){
+   font-size: 12px;
+   width: 210px;
+   span {
+     right: 10px;
+     letter-spacing: 0.5px;
+   }
   }
 `;
 
@@ -250,15 +146,17 @@ const BudgetsTransaction = () => {
   
   return (
     <BalanceContainer>
+        <BalanceWrapper>
       <TotalBalance>
-        <h1>
-        Balance:
-        <span>
-          { totalBalance > 1 ? `+ $${ totalBalance.toString().replace( /\B(?=(\d{3})+(?!\d))/g, ',' ) }` :
+           <h1> Balance: </h1>
+       
+            <h2>
+               { totalBalance > 1 ? `+ $${ totalBalance.toString().replace( /\B(?=(\d{3})+(?!\d))/g, ',' ) }` :
             ` $${ totalBalance.toString().replace( /\B(?=(\d{3})+(?!\d))/g, ',' ) }` }
-        </span>
-      </h1>
+         </h2>
+       
       </TotalBalance>
+         </BalanceWrapper>
       <TotalContainer>
          <TotalIncome>
            <h3>
