4e586aa8cfca47ff64ea07f92192a8dc6849c3cd
Timothy Attah <timothyattah733@gmail.com>
2021-05-06 16:10:46 +0100

    Added user authentication to income budgets

--- client/src/pages/budgets/IncomeBudgets.js
+++ client/src/pages/budgets/IncomeBudgets.js
@@ -4,6 +4,7 @@ import { listIncomes, deleteIncome } from '../../redux/actions/incomesAction';
 import { Delete, Edit } from '@material-ui/icons';
 import { IconButton } from '@material-ui/core';
 import { BudgetsContainer, Budgets, BudgetsButton, BudgetsItems } from './IncomeExpensesStyles';
+import {user} from '../../components/NameInitials'
 
 
 const IncomeBudgets = () => {
@@ -14,28 +15,39 @@ const IncomeBudgets = () => {
 
   const incomes = useSelector( state => state.incomesReducer.incomes );
   console.log( incomes );
+  console.log(user);
  
   return (
+    <div>
+      {user.results || incomes.postedBy ? (
+
     <BudgetsContainer>
       <h1>Income Transaction</h1>
-      {incomes.length ? (
-        incomes.map( income => {
-          return (
-            <Budgets key={ income._id }>
-              <li>
-                <BudgetsItems>{ income.item }: <span>{ income.value }</span></BudgetsItems>
-                <BudgetsButton>
-                  <IconButton><Edit /></IconButton>
-                  <IconButton onClick={ () => dispatch( deleteIncome( income._id ) ) }> <Delete /></IconButton>
-                </BudgetsButton>
-              </li>
-            </Budgets>
-          );
-        } )
-      ) : (
-        <h2>Loading...</h2>
-      ) }
+          {incomes.length ? (
+            incomes.map( income => {
+              return (
+                income.postedBy._id && user.results._id && (
+                  <Budgets key={ income._id }>
+                  <li>
+                    <BudgetsItems>{ income.item }: <span>{ income.value }</span></BudgetsItems>
+                    <BudgetsButton>
+                      <IconButton><Edit /></IconButton>
+                      <IconButton onClick={ () => dispatch( deleteIncome( income._id ) ) }> <Delete /></IconButton>
+                    </BudgetsButton>
+                  </li>
+                </Budgets>
+                )
+                
+              );
+            } )
+          ) : (
+            <h2>Loading...</h2>
+          ) }
     </BudgetsContainer>
+      ): (
+        <h2>Loading...</h2>
+      )}
+    </div>
   );
 };
 
