601e68805d5e4c6854f3e9ef2e63b143c7af3189
Timothy Attah <timothyattah733@gmail.com>
2021-05-07 01:32:48 +0100

    Made some changes

--- client/src/redux/api/notesApi.js
+++ client/src/redux/api/notesApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 
-const API = axios.create( { baseURL: process.env.REACT_APP_MY_API_KEY } )
+const baseURL = process.env.REACT_APP_BACKEND_API;
+const API = axios.create( { baseURL: baseURL } )
 
 API.interceptors.request.use( ( req ) => {
   if ( localStorage.getItem( 'jwt' ) ) {
