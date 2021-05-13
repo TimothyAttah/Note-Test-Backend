09def888d9305c5ca684bce46041be527f688f9d
Timothy Attah <timothyattah733@gmail.com>
2021-05-13 06:04:10 +0100

    Added deleteTodo route

--- controllers/todosController.js
+++ controllers/todosController.js
@@ -23,7 +23,7 @@ exports.createTodos = async ( req, res ) => {
 
 exports.getAllTodos = async ( req, res ) => {
   try {
-    const todos = await Todos.find().populate( 'postedBy', '-password' );
+    const todos = await Todos.find().sort({date: -1}).populate( 'postedBy', '-password' );
     res.status(200).json({message: 'All todos...', todos})
   } catch (error) {
     return res.status( 500 ).json( { error: error } );
@@ -32,10 +32,30 @@ exports.getAllTodos = async ( req, res ) => {
 
 exports.myTodos = async ( req, res ) => {
   try {
-    const todos = await Todos.find( { postedBy: req.user._id } )
+    const todos = await Todos.find( { postedBy: req.user._id } ).sort({date: -1})
       .populate( 'postedBy', '-password' );
     res.status(200).json({message: 'My todos!!!', todos})
   } catch ( error ) {
+    return res.status( 500 ).json( { error: error } );
+  }
+}
+
+exports.deleteTodos = async ( req, res ) => {
+  try {
+   const todos =  await Todos.findOne( { _id: req.params.todosId } )
+      .populate( 'postedBy', '-password' )
+      .exec( async ( err, todo ) => {
+        if ( err , !todo ) {
+          return res.status(404).json({error: err.message})
+        }
+        if ( !todos )
+          return res.status(404).json({error: 'Todos not found...'})
+        if ( todo.postedBy._id.toString() === req.user._id.toString() ) {
+          const deletedTodo = await todo.remove();
+          res.status(200).json({message: 'Todo deleted', deletedTodo})
+        }
+      })
+  } catch (error) {
     return res.status( 500 ).json( { error: error } );
   }
 }
--- routes/todosRoutes.js
+++ routes/todosRoutes.js
@@ -1,7 +1,7 @@
 const router = require( 'express' ).Router();
 const auth = require( '../middlewares/auth' );
 const {
-  createTodos, getAllTodos, myTodos
+  createTodos, getAllTodos, myTodos, deleteTodos
 } = require( '../controllers/todosController' );
 
 router.post( '/create', auth, createTodos );
@@ -9,5 +9,7 @@ router.post( '/create', auth, createTodos );
 router.get( '/', getAllTodos );
 
 router.get( '/users/todo', auth, myTodos );
+
+router.delete( '/:todosId/delete', auth, deleteTodos );
 
 module.exports = router;
