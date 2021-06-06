39893067746f102c321cc91d89a15798745d2acd
Timothy Attah <timothyattah733@gmail.com>
2021-06-06 20:11:53 +0100

    Formated UserProfile and TodosItem.js

--- client/src/pages/profile/UserProfile.js
+++ client/src/pages/profile/UserProfile.js
@@ -1,358 +1,391 @@
-import React, {useEffect, useState} from 'react';
-import {  Button, Divider, Fab } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Button, Divider, Fab } from '@material-ui/core';
 import { user } from '../../components/NamesInitials';
 import styled, { css } from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import ReadMore from '../../components/ReadMore';
 
-
 const Profiles = styled.div`
-  display: flex;
-  justify-content: space-around;
-  @media (max-width: 700px){
-    display: block;
-  padding-left: 200px;
-  }
-  @media (max-width: 545px){
-    padding-left: 100px;
-  }
-  @media (max-width: 450px){
-    padding-left: 30px;
-  }
+	display: flex;
+	justify-content: space-around;
+	@media (max-width: 700px) {
+		display: block;
+		padding-left: 200px;
+	}
+	@media (max-width: 545px) {
+		padding-left: 100px;
+	}
+	@media (max-width: 450px) {
+		padding-left: 30px;
+	}
 `;
 
 const ProfileCardIcon = styled.div`
-  border: 2px solid green;
-  width: 250px;
-  height: 300px;
-  ${props => props.primary && css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .MuiAvatar-colorDefault{
-      background-color: #fff;
-    }
-    .MuiAvatar-root {
-      width: 150px;
-      height: 150px;
-      border: 2px solid #bdbdbd;
-     img {
-       object-fit: contain;
-       max-width: 100%;
-     }
-    }
-  `}
-  @media (max-width: 600px){
-   width: 200px;
-   height: 200px;
-     .MuiAvatar-root {
-      width: 100px;
-      height: 100px;
-    }
-  }
+	border: 2px solid green;
+	width: 250px;
+	height: 300px;
+	${props =>
+		props.primary &&
+		css`
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			.MuiAvatar-colorDefault {
+				background-color: #fff;
+			}
+			.MuiAvatar-root {
+				width: 150px;
+				height: 150px;
+				border: 2px solid #bdbdbd;
+				img {
+					object-fit: contain;
+					max-width: 100%;
+				}
+			}
+		`}
+	@media (max-width: 600px) {
+		width: 200px;
+		height: 200px;
+		.MuiAvatar-root {
+			width: 100px;
+			height: 100px;
+		}
+	}
 `;
 
 const ProfileRight = styled.div`
- width: 500px;
- ${props => props.primary && css`
-    display: flex;
-    justify-content: space-between;
-    padding: 50px 0;
-    h4 {
-      color: #bdbdbd;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      flex-direction: column;
-      height: 85px;
-    }
-    span {
-      color: #fff;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    @media (max-width: 600px){
-      h4{
-        height: 65px;
-      }
-      .MuiFab-root {
-        width: 35px;
-        height: 35px;    
-      }
- };
- @media (max-width: 450px){
-   padding: 30px 0;
-    h4 {
-      width: 100px;
-      font-size: 12px;
-      font-weight: bolder;
-      padding-right: 15px;
-      align-items: flex-start;
-    }
-  }
- `};
- @media (max-width: 850px){
-   width: 300px;
- };
+	width: 500px;
+	${props =>
+		props.primary &&
+		css`
+			display: flex;
+			justify-content: space-between;
+			padding: 50px 0;
+			h4 {
+				color: #bdbdbd;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				flex-direction: column;
+				height: 85px;
+			}
+			span {
+				color: #fff;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+			@media (max-width: 600px) {
+				h4 {
+					height: 65px;
+				}
+				.MuiFab-root {
+					width: 35px;
+					height: 35px;
+				}
+			}
+			@media (max-width: 450px) {
+				padding: 30px 0;
+				h4 {
+					width: 100px;
+					font-size: 12px;
+					font-weight: bolder;
+					padding-right: 15px;
+					align-items: flex-start;
+				}
+			}
+		`};
+	@media (max-width: 850px) {
+		width: 300px;
+	}
 
- @media (max-width: 450px){
-   h1 {font-size: 20px;}
-   h4{font-size: 12px;}
- };
-  @media (max-width: 300px){
-    width: 150px;
- };
-`
+	@media (max-width: 450px) {
+		h1 {
+			font-size: 20px;
+		}
+		h4 {
+			font-size: 12px;
+		}
+	}
+	@media (max-width: 300px) {
+		width: 150px;
+	} ;
+`;
 
 const ProfilePostContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
-  padding-top: 20px;
+	width: 100%;
+	display: flex;
+	justify-content: space-evenly;
+	flex-wrap: wrap;
+	padding-top: 20px;
 `;
 
 const ProfilePost = styled.div`
-  width: 400px;
-  height: 300px;
-  border: 2px solid gray;
-  position: relative;
-  border-radius: 5px;
-  h2 {
-    padding: 20px 10px 10px 10px;
-  }
-   @media (max-width:450px){
-    width: 250px;
-    h2 {
-      font-size: 18px;
-    }
-  }
+	width: 400px;
+	height: 300px;
+	border: 2px solid gray;
+	position: relative;
+	border-radius: 5px;
+	h2 {
+		padding: 20px 10px 10px 10px;
+	}
+	@media (max-width: 450px) {
+		width: 250px;
+		h2 {
+			font-size: 18px;
+		}
+	}
 `;
 
 const AvatarContainer = styled.div`
-  width: 200px;
-  height: 200px;
-  overflow: hidden;
-  position: relative;
-  margin: 15px auto;
-  border: 1px solid #ddd;
-  border-radius: 50%;
-  cursor: pointer;
-  img {
-    width: 100%;
-    height:100%;
-    display: block;
-    object-fit: cover;
-  }
-  span {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 50%;
-    background: #fff8;
-    text-align: center;
-    text-transform: uppercase;
-    font-weight: 400;
-    color: rgb(225, 140, 45);
-    transition: 0.3s ease-in-out;
-    :hover {
-      bottom: -15%;
-      display: block;
-      background: red;
-    }
-    #file_upload {
-      position: absolute;
-      top: 0;
-      left:0;
-      width: 100%;
-      height: 100%;
-      cursor: pointer;
-      opacity: 0;
-    }
-  }
+	width: 200px;
+	height: 200px;
+	overflow: hidden;
+	position: relative;
+	margin: 15px auto;
+	border: 1px solid #ddd;
+	border-radius: 50%;
+	cursor: pointer;
+	img {
+		width: 100%;
+		height: 100%;
+		display: block;
+		object-fit: cover;
+	}
+	span {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 50%;
+		background: #fff8;
+		text-align: center;
+		text-transform: uppercase;
+		font-weight: 400;
+		color: rgb(225, 140, 45);
+		transition: 0.3s ease-in-out;
+		:hover {
+			bottom: -15%;
+			display: block;
+			background: red;
+		}
+		#file_upload {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			cursor: pointer;
+			opacity: 0;
+		}
+	}
 `;
-
-
 
 const ProfilePostContent = styled.div`
-padding: 10px 10px 20px 10px;
-
+	padding: 10px 10px 20px 10px;
 `;
-
 
 const ProfilePostButton = styled.div`
-padding-top: 50px;
-position: absolute;
-bottom: 20px;
-right: 10px;
+	padding-top: 50px;
+	position: absolute;
+	bottom: 20px;
+	right: 10px;
 `;
 
-
-
-
 const UserProfile = () => {
-  const dispatch = useDispatch();
-  const [ userProfile, setUserProfile ] = useState( null );
-  const { id } = useParams();
-  
-  useEffect( () => {
-     fetch( `/auth/users/${ id }/user`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer " + localStorage.getItem( 'jwt' )
-    }
-  } ).then( res => res.json() )
-    .then( data => {
-      if ( data.error ) {
-        console.log( data.error );
-      } else {
-        setUserProfile( data );
-    }
-    } ).catch( err => {
-    console.log(err);
-  })
-  }, [ dispatch, id ] );
+	const dispatch = useDispatch();
+	const [userProfile, setUserProfile] = useState(null);
+	const { id } = useParams();
 
-  const [ showFollow, setShowFollow ] = useState( user && user.results ? !user.results.following.includes( id ) : true );
- 
-  const fullName = `${userProfile && userProfile.user.firstName } ${ userProfile && userProfile.user.lastName }`
+	useEffect(() => {
+		fetch(`/auth/users/${id}/user`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+			},
+		})
+			.then(res => res.json())
+			.then(data => {
+				if (data.error) {
+					console.log(data.error);
+				} else {
+					setUserProfile(data);
+				}
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}, [dispatch, id]);
 
-  //http://localhost:8080/auth/users/follow
-  const followUser = () => {
-    fetch( `/auth/users/follow`, {
-      method: 'PUT',
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + localStorage.getItem( 'jwt' )
-      },
-      body: JSON.stringify( { followId: id } )
-    } ).then( res => res.json() )
-      .then( data => {
-        console.log( data );
-        localStorage.setItem( 'user', JSON.stringify( data ) );
-        setUserProfile( ( prevState ) => {
-          return {
-            ...prevState,
-            user: {
-              ...prevState.user,
-              followers: [ ...prevState.user.followers, data._id ]
-            }
-          }
-        } );
-        setShowFollow( false );
-      } )
-      .catch( err => {
-        console.log( err );
-      } )
-  };
+	const [showFollow, setShowFollow] = useState(
+		user && user.results ? !user.results.following.includes(id) : true
+	);
 
-  const unFollowUser = () => {
-    fetch( `/auth/users/unfollow`, {
-      method: 'PUT',
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + localStorage.getItem( 'jwt' )
-      },
-      body: JSON.stringify( { unfollowId: id } )
-    } ).then( res => res.json() )
-      .then( data => {
-        localStorage.setItem( 'user', JSON.stringify( data ) );
-        setUserProfile( ( prevState ) => {
-          const newFollower = prevState.user.followers.filter( item => item !== data._id );
-          return {
-            ...prevState,
-            user: {
-              ...prevState.user,
-              followers: newFollower
-            }
-          }
-        } );
-        setShowFollow( true );
-        //  window.location.reload( false );
-      } )
-      .catch( err => {
-        console.log( err );
-      } )
-  };
+	const fullName = `${userProfile && userProfile.user.firstName} ${
+		userProfile && userProfile.user.lastName
+	}`;
 
-  console.log( userProfile );
+	//http://localhost:8080/auth/users/follow
+	const followUser = () => {
+		fetch(`/auth/users/follow`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+			},
+			body: JSON.stringify({ followId: id }),
+		})
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				localStorage.setItem('user', JSON.stringify(data));
+				setUserProfile(prevState => {
+					return {
+						...prevState,
+						user: {
+							...prevState.user,
+							followers: [...prevState.user.followers, data._id],
+						},
+					};
+				});
+				setShowFollow(false);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
 
-  return (
-    <>
-      {userProfile ? (
-        <>
-          <Profiles>
-            <ProfileCardIcon>
-              <ProfileCardIcon primary>
-                
-                 <AvatarContainer>
-                  <img src={ userProfile.user.avatar} alt=''/>
-                </AvatarContainer>
-              </ProfileCardIcon>
-        
-            </ProfileCardIcon>
+	const unFollowUser = () => {
+		fetch(`/auth/users/unfollow`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+			},
+			body: JSON.stringify({ unfollowId: id }),
+		})
+			.then(res => res.json())
+			.then(data => {
+				localStorage.setItem('user', JSON.stringify(data));
+				setUserProfile(prevState => {
+					const newFollower = prevState.user.followers.filter(
+						item => item !== data._id
+					);
+					return {
+						...prevState,
+						user: {
+							...prevState.user,
+							followers: newFollower,
+						},
+					};
+				});
+				setShowFollow(true);
+				//  window.location.reload( false );
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
 
-            <ProfileRight>
-              <>
-                <h1>{ fullName }</h1>
-                <h4>{ userProfile.user.email }</h4>
-              </>
-              <Divider />
-              { userProfile.posts && (
-                <ProfileRight primary>
-                  <h4><span><Fab color='secondary'>{ userProfile.posts.length }</Fab></span>Posts</h4>
-                  <h4><span><Fab color='secondary'>{ userProfile.user.followers.length }</Fab></span>Followers</h4>
-                  <h4><span><Fab color='secondary'>{ userProfile.user.following.length }</Fab></span>Following</h4>
-                </ProfileRight>
-              ) }
-              <div>
-                { showFollow ? (
-                  <Button variant='contained' color='primary' onClick={ () => followUser() }>Follow</Button>
-                ) : (
-                  <Button variant='contained' color='primary' onClick={ () => unFollowUser() }>Unfollow</Button>
-                )
-                }
-              </div>
-              
-            </ProfileRight>
-          </Profiles>
-          <Divider />
-          <ProfilePostContainer>
-            {
-              userProfile.posts.map( note => {
-                return (
-                   <div key={note._id} style={{paddingBottom: '30px'}}>
-                <ProfilePost >
-                  <h2>{ note.title }</h2>
-                  <Divider />
-                  <ProfilePostContent>
-                     <ReadMore>
-                  { note.content }
-                    </ReadMore>
-                </ProfilePostContent>
-                  <ProfilePostButton>
-                     <Button variant='contained' size='small' color='primary'>
-                      <Link to={ `/api/users/notes/${ note._id }/note/read` }>Read More</Link>
-                    </Button>
-                 </ProfilePostButton>
-                   
-                  
-              </ProfilePost>
-              </div>
-                );
-              } )
-            }
-          </ProfilePostContainer>
-        </>
-      ) : (
-        <h2>Loading profile...</h2>
-      ) }
-    </>
-  );
+	console.log(userProfile);
+
+	return (
+		<>
+			{userProfile ? (
+				<>
+					<Profiles>
+						<ProfileCardIcon>
+							<ProfileCardIcon primary>
+								<AvatarContainer>
+									<img src={userProfile.user.avatar} alt='' />
+								</AvatarContainer>
+							</ProfileCardIcon>
+						</ProfileCardIcon>
+
+						<ProfileRight>
+							<>
+								<h1>{fullName}</h1>
+								<h4>{userProfile.user.email}</h4>
+							</>
+							<Divider />
+							{userProfile.posts && (
+								<ProfileRight primary>
+									<h4>
+										<span>
+											<Fab color='secondary'>{userProfile.posts.length}</Fab>
+										</span>
+										Posts
+									</h4>
+									<h4>
+										<span>
+											<Fab color='secondary'>
+												{userProfile.user.followers.length}
+											</Fab>
+										</span>
+										Followers
+									</h4>
+									<h4>
+										<span>
+											<Fab color='secondary'>
+												{userProfile.user.following.length}
+											</Fab>
+										</span>
+										Following
+									</h4>
+								</ProfileRight>
+							)}
+							<div>
+								{showFollow ? (
+									<Button
+										variant='contained'
+										color='primary'
+										onClick={() => followUser()}
+									>
+										Follow
+									</Button>
+								) : (
+									<Button
+										variant='contained'
+										color='primary'
+										onClick={() => unFollowUser()}
+									>
+										Unfollow
+									</Button>
+								)}
+							</div>
+						</ProfileRight>
+					</Profiles>
+					<Divider />
+					<ProfilePostContainer>
+						{userProfile.posts.map(note => {
+							return (
+								<div key={note._id} style={{ paddingBottom: '30px' }}>
+									<ProfilePost>
+										<h2>{note.title}</h2>
+										<Divider />
+										<ProfilePostContent>
+											<ReadMore>{note.content}</ReadMore>
+										</ProfilePostContent>
+										<ProfilePostButton>
+											<Button variant='contained' size='small' color='primary'>
+												<Link to={`/api/users/notes/${note._id}/note/read`}>
+													Read More
+												</Link>
+											</Button>
+										</ProfilePostButton>
+									</ProfilePost>
+								</div>
+							);
+						})}
+					</ProfilePostContainer>
+				</>
+			) : (
+				<h2>Loading profile...</h2>
+			)}
+		</>
+	);
 };
 
 export default UserProfile;
--- client/src/pages/todos/TodoItem.js
+++ client/src/pages/todos/TodoItem.js
@@ -7,154 +7,167 @@ import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { checkTodos, todosDelete } from '../../redux/actions/todosAction';
 
-
 const Todos = styled.ul`
-  margin: 0;
-  padding: 0;
-  .status {
-    background-color: #3d3d3d;
-    color: #fff;
-    text-decoration: line-through;
-    .MuiCheckbox-root{
-      color: #fff;
-    }
-      h4, p {
-         opacity: 0.7;
-      }
-  }
-  li {
-    display: flex;
-    justify-content: space-between;
-    list-style: none;
-  margin: 15px 0;
-  padding: 10px;
-  text-transform: capitalize;
-  @media (max-width: 411px){
-  flex-direction: column;
-}
- .PrivateSwitchBase-input-4{
-      border: 2px solid red;
-     input {
-       background-color: white;
-       color: -internal-light-dark(white, black);
-     }
-    }
-  p {
-    color: #909090;
-    font-weight: 500;
-    font-size: 15px;
-    display: flex;
-    justify-items: flex-end;
-    padding-bottom: 10px;
-  }
- 
-   box-shadow:  -5px -5px 5px #fff7,
-              5px 5px 5px #0002;
-  animation: opacity 0.2s linear;
-  @keyframes opacity{
-  from{
-    opacity: 0;
-    transform: scale(0.7);
-  }
-}
-  }
+	margin: 0;
+	padding: 0;
+	.status {
+		background-color: #3d3d3d;
+		color: #fff;
+		text-decoration: line-through;
+		.MuiCheckbox-root {
+			color: #fff;
+		}
+		h4,
+		p {
+			opacity: 0.7;
+		}
+	}
+	li {
+		display: flex;
+		justify-content: space-between;
+		list-style: none;
+		margin: 15px 0;
+		padding: 10px;
+		text-transform: capitalize;
+		@media (max-width: 411px) {
+			flex-direction: column;
+		}
+		.PrivateSwitchBase-input-4 {
+			border: 2px solid red;
+			input {
+				background-color: white;
+				color: -internal-light-dark(white, black);
+			}
+		}
+		p {
+			color: #909090;
+			font-weight: 500;
+			font-size: 15px;
+			display: flex;
+			justify-items: flex-end;
+			padding-bottom: 10px;
+		}
 
+		box-shadow: -5px -5px 5px #fff7, 5px 5px 5px #0002;
+		animation: opacity 0.2s linear;
+		@keyframes opacity {
+			from {
+				opacity: 0;
+				transform: scale(0.7);
+			}
+		}
+	}
 `;
 
 const TodoName = styled.div`
-  display: flex;
- justify-content: center;
- h4 {
-   padding: 10px 0;
- }
- @media (max-width: 411px){
-   justify-content: flex-start;
- }
+	display: flex;
+	justify-content: center;
+	h4 {
+		padding: 10px 0;
+	}
+	@media (max-width: 411px) {
+		justify-content: flex-start;
+	}
 `;
 
 const TodoIcons = styled.div`
-  display: flex;
-  align-items: flex-end;
-  justify-content:flex-end;
-  padding-top: 70px;
-  .MuiSvgIcon-root{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  @media (max-width: 411px){
-    padding-top: 20px;
-  }
+	display: flex;
+	align-items: flex-end;
+	justify-content: flex-end;
+	padding-top: 70px;
+	.MuiSvgIcon-root {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	@media (max-width: 411px) {
+		padding-top: 20px;
+	}
 `;
 
-const TodoItem = ( { todo } ) => {
-  const dispatch = useDispatch();
-  const [isComplete ,setIsComplete ] = useState( todo.isComplete );
+const TodoItem = ({ todo }) => {
+	const dispatch = useDispatch();
+	const [isComplete, setIsComplete] = useState(todo.isComplete);
 
-  const handleEdit = () => {
-    window.scrollTo( {
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    } )
-  };
+	const handleEdit = () => {
+		window.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'smooth',
+		});
+	};
 
-  return (
-    <div>
-      <Todos>
-        { todo.isComplete ? (
-           <li className='status'>
-          <TodoName>
-              <div>
-              <Checkbox
-              color={todo.isComplete ? 'default' : 'primary'}
-              checked={ isComplete }
-              onChange={ () => setIsComplete(dispatch(checkTodos(todo, todo._id))) }
-              inputProps={{'aria-label': 'secondary checkbox'}}
-            />
-             </div>
-              <div>
-              <h4>{ todo.name }</h4>
-              <p>Added: { moment(todo.date).calendar()}</p>
-              </div>
-            </TodoName>
-            <Divider />
-          <TodoIcons>
-            <ButtonGroup size='small'>
-              <Button onClick={handleEdit}><Link to={`/api/users/todos/${todo._id}/edit`}><Create color='primary' /></Link></Button>
-              <Button onClick={()=> dispatch(todosDelete(todo._id))}><Delete color='secondary' /></Button>
-            </ButtonGroup>
-          </TodoIcons>
-        </li>
-        ) : (
-             <li>
-         <TodoName>
-              <div>
-              <Checkbox
-              color={todo.isComplete ? 'default' : 'primary'}
-              checked={ todo.isComplete }
-              onChange={ () => setIsComplete(dispatch(checkTodos(todo, todo._id))) }
-              inputProps={{'aria-label': 'secondary checkbox'}}
-            />
-             </div>
-              <div>
-              <h4>{ todo.name }</h4>
-              <p>Added: { moment(todo.date).fromNow()}</p>
-              </div>
-              </TodoName>
-               <Divider />
-          <TodoIcons>
-            <ButtonGroup size='small'>
-              <Button onClick={handleEdit}><Link to={`/api/users/todos/${todo._id}/edit`}><Create color='primary' /></Link></Button>
-              <Button onClick={()=> dispatch(todosDelete(todo._id))}><Delete color='secondary' /></Button>
-            </ButtonGroup>
-          </TodoIcons>
-        </li>
-        )}
-       
-      </Todos>
-    </div>
-  );
+	return (
+		<div>
+			<Todos>
+				{todo.isComplete ? (
+					<li className='status'>
+						<TodoName>
+							<div>
+								<Checkbox
+									color={todo.isComplete ? 'default' : 'primary'}
+									checked={isComplete}
+									onChange={() =>
+										setIsComplete(dispatch(checkTodos(todo, todo._id)))
+									}
+									inputProps={{ 'aria-label': 'secondary checkbox' }}
+								/>
+							</div>
+							<div>
+								<h4>{todo.name}</h4>
+								<p>Added: {moment(todo.date).calendar()}</p>
+							</div>
+						</TodoName>
+						<Divider />
+						<TodoIcons>
+							<ButtonGroup size='small'>
+								<Button onClick={handleEdit}>
+									<Link to={`/api/users/todos/${todo._id}/edit`}>
+										<Create color='primary' />
+									</Link>
+								</Button>
+								<Button onClick={() => dispatch(todosDelete(todo._id))}>
+									<Delete color='secondary' />
+								</Button>
+							</ButtonGroup>
+						</TodoIcons>
+					</li>
+				) : (
+					<li>
+						<TodoName>
+							<div>
+								<Checkbox
+									color={todo.isComplete ? 'default' : 'primary'}
+									checked={todo.isComplete}
+									onChange={() =>
+										setIsComplete(dispatch(checkTodos(todo, todo._id)))
+									}
+									inputProps={{ 'aria-label': 'secondary checkbox' }}
+								/>
+							</div>
+							<div>
+								<h4>{todo.name}</h4>
+								<p>Added: {moment(todo.date).fromNow()}</p>
+							</div>
+						</TodoName>
+						<Divider />
+						<TodoIcons>
+							<ButtonGroup size='small'>
+								<Button onClick={handleEdit}>
+									<Link to={`/api/users/todos/${todo._id}/edit`}>
+										<Create color='primary' />
+									</Link>
+								</Button>
+								<Button onClick={() => dispatch(todosDelete(todo._id))}>
+									<Delete color='secondary' />
+								</Button>
+							</ButtonGroup>
+						</TodoIcons>
+					</li>
+				)}
+			</Todos>
+		</div>
+	);
 };
 
 export default TodoItem;
