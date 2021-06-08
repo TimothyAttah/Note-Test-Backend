8f0c120001497447bde9385d3927110e5460c0ff
Timothy Attah <timothyattah733@gmail.com>
2021-06-08 19:50:09 +0100

    Did some clean ups in notesReducer.js file

--- client/src/redux/reducer/notesReducer.js
+++ client/src/redux/reducer/notesReducer.js
@@ -1,43 +1,57 @@
-import { FRIENDS_NOTES, NOTES_COMMENTS, NOTES_CREATE, NOTES_DELETE, NOTES_EDIT, NOTES_LIKE, NOTES_UNLIKE, USERS_NOTES_LISTS, USER_NOTES_LISTS } from '../type';
+import {
+	FRIENDS_NOTES,
+	NOTES_COMMENTS,
+	NOTES_CREATE,
+	NOTES_DELETE,
+	NOTES_EDIT,
+	NOTES_LIKE,
+	NOTES_UNLIKE,
+	USERS_NOTES_LISTS,
+	USER_NOTES_LISTS,
+} from '../type';
 
 const initialState = {
-  notes: []
-}
+	notes: [],
+};
 
-const notesReducer = ( state = initialState, action ) => {
-  switch ( action.type ) {
-    case USERS_NOTES_LISTS:
-    case USER_NOTES_LISTS:
-    case FRIENDS_NOTES:
-      return {
-        ...state,
-        notes: action.payload
-      };
-    case NOTES_CREATE:
-      return {
-        ...state,
-        notes: [ action.payload, ...state.notes ]
-      }
-    case NOTES_DELETE:
-      return {
-        ...state,
-        notes: state.notes.filter( note => note._id !== action.payload )
-      }
-    case NOTES_EDIT:
-      return {
-        ...state,
-        notes: state.notes.map( note => note._id === action.payload._id ? action.payload : note )
-      }
-    case NOTES_LIKE:
-    case NOTES_UNLIKE:
-    case NOTES_COMMENTS:
-      return {
-        ...state,
-        notes: state.notes.map( note => note._id === action.payload._id ? action.payload : note )
-      };
-    default:
-      return state
-  }
-}
+const notesReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case USERS_NOTES_LISTS:
+		case USER_NOTES_LISTS:
+		case FRIENDS_NOTES:
+			return {
+				...state,
+				notes: action.payload,
+			};
+		case NOTES_CREATE:
+			return {
+				...state,
+				notes: [action.payload, ...state.notes],
+			};
+		case NOTES_DELETE:
+			return {
+				...state,
+				notes: state.notes.filter(note => note._id !== action.payload),
+			};
+		case NOTES_EDIT:
+			return {
+				...state,
+				notes: state.notes.map(note =>
+					note._id === action.payload._id ? action.payload : note
+				),
+			};
+		case NOTES_LIKE:
+		case NOTES_UNLIKE:
+		case NOTES_COMMENTS:
+			return {
+				...state,
+				notes: state.notes.map(note =>
+					note._id === action.payload._id ? action.payload : note
+				),
+			};
+		default:
+			return state;
+	}
+};
 
 export default notesReducer;
