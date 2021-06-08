e5113054df26a3fe03f50b2c44238d78b0d98a1a
Timothy Attah <timothyattah733@gmail.com>
2021-06-08 19:36:41 +0100

    Formated expensesAction code

--- client/src/redux/actions/expensesActions.js
+++ client/src/redux/actions/expensesActions.js
@@ -1,55 +1,60 @@
 import { toast } from 'react-toastify';
-import { CREATE_EXPENSES, DELETE_EXPENSES, EDIT_EXPENSES, LIST_EXPENSES } from '../type';
+import {
+	CREATE_EXPENSES,
+	DELETE_EXPENSES,
+	EDIT_EXPENSES,
+	LIST_EXPENSES,
+} from '../type';
 import * as api from '../api/notesApi';
 
-
-export const createExpenses = ( expensesData ) => async dispatch => {
-  try {
-    const { data } = await api.createExpenses( expensesData );
-    toast.success( data.message )
-    dispatch( {
-      type: CREATE_EXPENSES,
-      payload: data.newExpenses
-    } )
-  } catch ( err ) {
-    if ( err.response && err.response.data ) {
-      return toast.error(err.response.data.error)
-    }
-  }
+export const createExpenses = expensesData => async dispatch => {
+	try {
+		const { data } = await api.createExpenses(expensesData);
+		toast.success(data.message);
+		dispatch({
+			type: CREATE_EXPENSES,
+			payload: data.newExpenses,
+		});
+	} catch (err) {
+		if (err.response && err.response.data) {
+			return toast.error(err.response.data.error);
+		}
+	}
 };
 
 export const listExpenses = () => async dispatch => {
-  try {
-    const { data } = await api.getExpenses();
-    dispatch( {
-      type: LIST_EXPENSES,
-      payload: data.expenses
-    })
-  } catch ( error ) {
-    console.log( error );
-  }
+	try {
+		const { data } = await api.getExpenses();
+		dispatch({
+			type: LIST_EXPENSES,
+			payload: data.expenses,
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 
-export const deleteExpenses = ( expensesId ) => async dispatch => {
-  try {
-    toast.success( 'Expenses Deleted Successfully' )
-    await api.deleteExpenses(expensesId)
-    dispatch( {
-      type: DELETE_EXPENSES,
-      payload: expensesId
-    })
-  } catch (error) {
-    console.log(error);
-  }
-}
-export const editExpenses = ( expenses, id ) => async dispatch => {
-  try {
-    toast.success('Expenses edited successfully!')
-    dispatch( {
-      type: EDIT_EXPENSES,
-      payload: expenses, id
-    })
-  } catch (error) {
-    console.log(error);
-  }
-}
+export const deleteExpenses = expensesId => async dispatch => {
+	try {
+		toast.success('Expenses Deleted Successfully');
+		await api.deleteExpenses(expensesId);
+		dispatch({
+			type: DELETE_EXPENSES,
+			payload: expensesId,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+export const editExpenses = (expenses, id) => async dispatch => {
+	try {
+		toast.success('Expenses edited successfully!');
+		dispatch({
+			type: EDIT_EXPENSES,
+			payload: expenses,
+			id,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
