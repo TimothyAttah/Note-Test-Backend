3dc2b0603d207eb681e76df19d341a091abeef1c
Timothy Attah <timothyattah733@gmail.com>
2021-06-08 19:46:30 +0100

    Did some clean ups in expensesReducer.js file

--- client/src/redux/reducer/expensesReducer.js
+++ client/src/redux/reducer/expensesReducer.js
@@ -1,39 +1,43 @@
-import { CREATE_EXPENSES, DELETE_EXPENSES, EDIT_EXPENSES, LIST_EXPENSES } from '../type';
+import {
+	CREATE_EXPENSES,
+	DELETE_EXPENSES,
+	EDIT_EXPENSES,
+	LIST_EXPENSES,
+} from '../type';
 
 const initialState = {
-  expenses: [
-    // {id: v4(), item: 'Groceries', value: 2000},
-    // {id: v4(), item: 'house rent', value: 5000},
-    // {id: v4(), item: 'brought car', value: 1500},
-  ]
-}
+	expenses: [],
+};
 
-const ExpensesReducer = ( state = initialState, action ) => {
-  switch ( action.type ) {
-    case LIST_EXPENSES:
-      return {
-        ...state,
-        expenses: action.payload
-      }
-    case CREATE_EXPENSES:
-      return {
-        ...state,
-        expenses: [ action.payload, ...state.expenses ]
-      };
-    case DELETE_EXPENSES:
-      return {
-        ...state,
-        expenses: state.expenses.filter( expense=> expense._id !== action.payload )
-      };
-    case EDIT_EXPENSES:
-      return {
-        ...state,
-        expenses: state.expenses.map(expense=> expense.id === action.payload.id ? action.payload : expense)
-      }
-    default:
-      return state
-  }
-}
+const ExpensesReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case LIST_EXPENSES:
+			return {
+				...state,
+				expenses: action.payload,
+			};
+		case CREATE_EXPENSES:
+			return {
+				...state,
+				expenses: [action.payload, ...state.expenses],
+			};
+		case DELETE_EXPENSES:
+			return {
+				...state,
+				expenses: state.expenses.filter(
+					expense => expense._id !== action.payload
+				),
+			};
+		case EDIT_EXPENSES:
+			return {
+				...state,
+				expenses: state.expenses.map(expense =>
+					expense.id === action.payload.id ? action.payload : expense
+				),
+			};
+		default:
+			return state;
+	}
+};
 
 export default ExpensesReducer;
-
