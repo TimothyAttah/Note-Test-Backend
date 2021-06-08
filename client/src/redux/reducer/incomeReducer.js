5d290962dbe23fda75cb682e04af321bc1146a22
Timothy Attah <timothyattah733@gmail.com>
2021-06-08 19:47:58 +0100

    Did some clean ups in incomesReducer.js file

--- client/src/redux/reducer/incomeReducer.js
+++ client/src/redux/reducer/incomeReducer.js
@@ -1,40 +1,41 @@
-import { CREATE_INCOME, DELETE_INCOME, EDIT_INCOME, LIST_INCOMES } from '../type';
+import {
+	CREATE_INCOME,
+	DELETE_INCOME,
+	EDIT_INCOME,
+	LIST_INCOMES,
+} from '../type';
 
 const initialState = {
-  incomes: [
-    // {id: v4(), item: 'Salary', value: 5000},
-    // {id: v4(), item: 'Finished project', value: 2500},
-    // {id: v4(), item: 'Sold books', value: 1500},
-    // {id: v4(), item: 'made todos', value: 1000},
-  ]
-}
+	incomes: [],
+};
 
-const incomesReducer = ( state = initialState, action ) => {
-  switch ( action.type ) {
-    case LIST_INCOMES:
-      return {
-        ...state,
-        incomes: action.payload
-      }
-    case CREATE_INCOME:
-      return {
-        ...state,
-        incomes: [ action.payload, ...state.incomes ]
-      };
-    case DELETE_INCOME:
-      return {
-        ...state,
-        incomes: state.incomes.filter( income => income._id !== action.payload )
-      };
-    case EDIT_INCOME:
-      return {
-        ...state,
-        incomes: state.incomes.map(income => income.id === action.payload.id ? action.payload : income)
-      }
-    default:
-      return state
-  }
-}
+const incomesReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case LIST_INCOMES:
+			return {
+				...state,
+				incomes: action.payload,
+			};
+		case CREATE_INCOME:
+			return {
+				...state,
+				incomes: [action.payload, ...state.incomes],
+			};
+		case DELETE_INCOME:
+			return {
+				...state,
+				incomes: state.incomes.filter(income => income._id !== action.payload),
+			};
+		case EDIT_INCOME:
+			return {
+				...state,
+				incomes: state.incomes.map(income =>
+					income.id === action.payload.id ? action.payload : income
+				),
+			};
+		default:
+			return state;
+	}
+};
 
 export default incomesReducer;
-
