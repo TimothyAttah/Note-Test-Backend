57c9e41743e151949a45da85225c61d84488b484
Timothy Attah <timothyattah733@gmail.com>
2021-06-08 19:51:24 +0100

    Did some clean ups in todosReducer.js file

--- client/src/redux/reducer/todosReducer.js
+++ client/src/redux/reducer/todosReducer.js
@@ -1,38 +1,48 @@
-import { TODOS_CHECK, TODOS_CREATE, TODOS_DELETE, TODOS_EDIT, TODOS_LISTS } from '../type';
+import {
+	TODOS_CHECK,
+	TODOS_CREATE,
+	TODOS_DELETE,
+	TODOS_EDIT,
+	TODOS_LISTS,
+} from '../type';
 
 const initialState = {
-  todos: []
-}
+	todos: [],
+};
 
-const todosReducer = ( state = initialState, action ) => {
-  switch ( action.type ) {
-    case TODOS_LISTS:
-      return {
-        ...state,
-        todos: action.payload
-      };
-    case TODOS_CREATE:
-      return {
-        ...state,
-        todos: [ action.payload, ...state.todos ]
-      };
-    case TODOS_EDIT:
-      return {
-        ...state,
-        todos: state.todos.map( todo => todo._id === action.payload._id ? action.payload : todo )
-      };
-    case TODOS_DELETE:
-      return {
-        todos: state.todos.filter( todo => todo._id !== action.payload )
-      };
-    case TODOS_CHECK:
-      return {
-        ...state,
-        todos: state.todos.map( todo => todo._id === action.payload._id ? action.payload : todo )
-      };
-    default:
-      return state;
-  }
+const todosReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case TODOS_LISTS:
+			return {
+				...state,
+				todos: action.payload,
+			};
+		case TODOS_CREATE:
+			return {
+				...state,
+				todos: [action.payload, ...state.todos],
+			};
+		case TODOS_EDIT:
+			return {
+				...state,
+				todos: state.todos.map(todo =>
+					todo._id === action.payload._id ? action.payload : todo
+				),
+			};
+		case TODOS_DELETE:
+			return {
+				todos: state.todos.filter(todo => todo._id !== action.payload),
+			};
+		case TODOS_CHECK:
+			return {
+				...state,
+				todos: state.todos.map(todo =>
+					todo._id === action.payload._id ? action.payload : todo
+				),
+			};
+		default:
+			return state;
+	}
 };
 
 export default todosReducer;
