9456bd32ee709ff672861eecc89a60abf3c1b90f
Timothy Attah <timothyattah733@gmail.com>
2021-06-08 19:42:49 +0100

    Made some clean ups in usersAction.js file

--- client/src/redux/actions/usersActions.js
+++ client/src/redux/actions/usersActions.js
@@ -1,69 +1,71 @@
 import { FOLLOW_USER, GET_USER, UNFOLLOW_USER } from '../type';
 import * as api from '../api/notesApi';
 
-export const getUser = (id) => async dispatch => {
-  try {
-    const { data } = await api.getUser( id );
-    dispatch( {
-      type: GET_USER,
-      payload: data
-    })
-  } catch (error) {
-    console.log(error);
-  }
-}
+export const getUser = id => async dispatch => {
+	try {
+		const { data } = await api.getUser(id);
+		dispatch({
+			type: GET_USER,
+			payload: data,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 //http://localhost:8080/auth/users/follow
 
-export const followUsers = (id) => dispatch => {
-  fetch( `/auth/users/follow`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer "+localStorage.getItem('jwt')
-    },
-    body: JSON.stringify({followId: id})
-  } ).then( res => res.json() )
-    .then( data => {
-      if ( data.error ) {
-        console.log( data.error );
-      } else {
-        console.log( data );
-         localStorage.setItem('user', JSON.stringify(data))
-        dispatch( {
-          type: FOLLOW_USER,
-          payload: {following: data.following, followers: data.followers}
-        } )
-        // window.location.reload(false)
-    }
-    } ).catch( err => {
-    console.log(err);
-  })
-}
+export const followUsers = id => dispatch => {
+	fetch(`/auth/users/follow`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+		},
+		body: JSON.stringify({ followId: id }),
+	})
+		.then(res => res.json())
+		.then(data => {
+			if (data.error) {
+				console.log(data.error);
+			} else {
+				console.log(data);
+				localStorage.setItem('user', JSON.stringify(data));
+				dispatch({
+					type: FOLLOW_USER,
+					payload: { following: data.following, followers: data.followers },
+				});
+				// window.location.reload(false)
+			}
+		})
+		.catch(err => {
+			console.log(err);
+		});
+};
 
-export const unfollowUsers = (id) => dispatch => {
-  fetch( `/auth/users/unfollow`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer "+localStorage.getItem('jwt')
-    },
-    body: JSON.stringify({unfollowId: id})
-  } ).then( res => res.json() )
-    .then( data => {
-      if ( data.error ) {
-        console.log( data.error );
-      } else {
-        console.log( data );
-         localStorage.setItem('user', JSON.stringify(data))
-        dispatch( {
-          type: UNFOLLOW_USER,
-          payload: data.results
-        } )
-         
-    }
-    } ).catch( err => {
-    console.log(err);
-  })
-}
-
+export const unfollowUsers = id => dispatch => {
+	fetch(`/auth/users/unfollow`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+		},
+		body: JSON.stringify({ unfollowId: id }),
+	})
+		.then(res => res.json())
+		.then(data => {
+			if (data.error) {
+				console.log(data.error);
+			} else {
+				console.log(data);
+				localStorage.setItem('user', JSON.stringify(data));
+				dispatch({
+					type: UNFOLLOW_USER,
+					payload: data.results,
+				});
+			}
+		})
+		.catch(err => {
+			console.log(err);
+		});
+};
