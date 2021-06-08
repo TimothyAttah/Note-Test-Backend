b4ae75184b9ba28af7b5532aaa9d2207d2cc0b4a
Timothy Attah <timothyattah733@gmail.com>
2021-06-08 19:52:26 +0100

    Did some clean ups in usersReducer.js file

--- client/src/redux/reducer/usersReducer.js
+++ client/src/redux/reducer/usersReducer.js
@@ -1,34 +1,34 @@
 import { FOLLOW_USER, GET_USER, UNFOLLOW_USER } from '../type';
 
 const initialState = {
-  user: [],
-  followers: [],
-  following: []
-}
+	user: [],
+	followers: [],
+	following: [],
+};
 
-const usersReducer = ( state = initialState, action ) => {
-  switch ( action.type ) {
-    case GET_USER:
-      return {
-        ...state,
-        user: action.payload
-      }
-    case FOLLOW_USER:
-      return {
-        ...state,
-        followers: [action.payload.followers, ...state.user],
-        following: [action.payload.following, ...state.user] 
-      }
-    case UNFOLLOW_USER:
-      return {
-        ...state,
-        user: action.payload,
-        followers: state.followers.filter(item => item !== action.payload),
-        following: state.following.filter(item => item !== action.payload),
-      }
-    default:
-      return state
-  }
-}
+const usersReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case GET_USER:
+			return {
+				...state,
+				user: action.payload,
+			};
+		case FOLLOW_USER:
+			return {
+				...state,
+				followers: [action.payload.followers, ...state.user],
+				following: [action.payload.following, ...state.user],
+			};
+		case UNFOLLOW_USER:
+			return {
+				...state,
+				user: action.payload,
+				followers: state.followers.filter(item => item !== action.payload),
+				following: state.following.filter(item => item !== action.payload),
+			};
+		default:
+			return state;
+	}
+};
 
 export default usersReducer;
