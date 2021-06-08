d64b8ee1d6df5a881f4ed892df9e6f4847d82a06
Timothy Attah <timothyattah733@gmail.com>
2021-06-08 19:44:21 +0100

    Made some clean ups in authReducer.js file

--- client/src/redux/reducer/authReducer.js
+++ client/src/redux/reducer/authReducer.js
@@ -1,18 +1,16 @@
-import {
-  SIGNIN_USER, SIGNUP_USER, GET_USERS, LOGOUT_USER
-} from '../type';
+import { SIGNIN_USER, SIGNUP_USER, GET_USERS, LOGOUT_USER } from '../type';
 
-const auth = ( auth = [], action ) => {
-  switch ( action.type ) {
-    case SIGNUP_USER:
-    case SIGNIN_USER:
-      return [ action.payload, ...auth ];
-    case GET_USERS:
-    case LOGOUT_USER:
-      return action.payload;
-    default:
-      return auth
-  }
-}
+const auth = (auth = [], action) => {
+	switch (action.type) {
+		case SIGNUP_USER:
+		case SIGNIN_USER:
+			return [action.payload, ...auth];
+		case GET_USERS:
+		case LOGOUT_USER:
+			return action.payload;
+		default:
+			return auth;
+	}
+};
 
 export default auth;
