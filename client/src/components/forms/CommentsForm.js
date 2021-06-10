d8724f2a96a668b8d4342e2b12054ec730a59ced
Timothy Attah <timothyattah733@gmail.com>
2021-06-10 23:27:03 +0100

    Initial commit

--- client/src/components/forms/CommentsForm.js
+++ client/src/components/forms/CommentsForm.js
@@ -1,45 +1,45 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { notesComments } from '../../redux/actions/notesActions';
 
 const Form = styled.form`
-  display: flex;
-  width: 700px;
-  padding-top: 40px;
- .icon-image {
-   padding: 0px 10px;
- }
-  input {
-    width: 100%;
-    padding: 10px;
-    outline: none;
-    ::placeholder {
-      color: grey;
-    }
-  }
-  button{
-    display: none;
-  }
-`
+	display: flex;
+	width: 700px;
+	padding-top: 40px;
+	.icon-image {
+		padding: 0px 10px;
+	}
+	input {
+		width: 100%;
+		padding: 10px;
+		outline: none;
+		::placeholder {
+			color: grey;
+		}
+	}
+	button {
+		display: none;
+	}
+`;
 
-const CommentsForm = ( { src, note } ) => {
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <Form onSubmit={ ( e ) => {
-        e.preventDefault();
-        dispatch(notesComments(note._id, e.target[0].value))
-      }}>
-        <div className='icon-image'>{ src && <Avatar src={ src } /> }</div>
-        <input
-          placeholder='Write a comment...'
-        />
-        <button type='submit'></button>
-      </Form>
-    </div>
-  )
-}
+const CommentsForm = ({ src, note }) => {
+	const dispatch = useDispatch();
+	return (
+		<div>
+			<Form
+				onSubmit={e => {
+					e.preventDefault();
+					dispatch(notesComments(note._id, e.target[0].value));
+				}}
+			>
+				<div className='icon-image'>{src && <Avatar src={src} />}</div>
+				<input placeholder='Write a comment...' />
+				<button type='submit'></button>
+			</Form>
+		</div>
+	);
+};
 
 export default CommentsForm;
