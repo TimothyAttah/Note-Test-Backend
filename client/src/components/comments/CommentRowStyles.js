b8138cdf3df8af06b1512757abdb23281ea85d96
Timothy Attah <timothyattah733@gmail.com>
2021-06-10 23:22:22 +0100

    Initial commit

--- client/src/components/comments/CommentRowStyles.js
+++ client/src/components/comments/CommentRowStyles.js
@@ -1,49 +1,55 @@
 import styled, { css } from 'styled-components';
 
 export const Comments = styled.div`
-  display: flex;
-   width: 700px;
-   padding: 10px;
+	display: flex;
+	width: 700px;
+	padding: 10px;
 `;
 
 export const CommentsLeft = styled.div`
-  padding-right: 10px;
+	padding-right: 10px;
 `;
 
 export const CommentsTop = styled.div`
-  width: 600px;
-  ${ props => props.primary && css`
-   border: 1px solid #bdbdbd;
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
-   cursor: pointer;
-   border-radius: 5px;
-   padding: 0 10px;
-   h4 {
-     padding-bottom: 3px;
-   }
-   :hover {
-     .delete-icon {
-       display: block;
-       cursor: pointer;
-     }
-   }
-   .delete-icon {
-     display: none;
-   }
-  `}
+	width: 600px;
+	${props =>
+		props.primary &&
+		css`
+			border: 1px solid #bdbdbd;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			cursor: pointer;
+			border-radius: 5px;
+			padding: 0 10px;
+			h4 {
+				padding-bottom: 3px;
+			}
+			:hover {
+				.delete-icon {
+					display: block;
+					cursor: pointer;
+				}
+			}
+			.delete-icon {
+				display: none;
+			}
+		`}
 `;
 
 export const CommentsBottom = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  ${ props => props.primary && css`
-  width: 150px;
-  `}
-  ${ props => props.secondary && css`
-       display: flex;
-  align-items: center;
-  `}
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	${props =>
+		props.primary &&
+		css`
+			width: 150px;
+		`}
+	${props =>
+		props.secondary &&
+		css`
+			display: flex;
+			align-items: center;
+		`}
 `;
