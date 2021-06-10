007e5b8a9af037be45b8d3004f1ef6d32dbfbc5e
Timothy Attah <timothyattah733@gmail.com>
2021-06-10 23:20:16 +0100

    Initial commit

--- client/src/components/buttons/Button.js
+++ client/src/components/buttons/Button.js
@@ -3,22 +3,26 @@ import PropTypes from 'prop-types';
 import { Button, ButtonGroup } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
-const Buttons = ( { first, second, firstLink, secondLink } ) => {
-  return (
-    <>
-      <ButtonGroup variant='contained' color='primary'>
-        <Button><Link to={firstLink}>{ first }</Link></Button>
-        <Button><Link to={secondLink}>{ second }</Link></Button>
-      </ButtonGroup>
-    </>
-  );
+const Buttons = ({ first, second, firstLink, secondLink }) => {
+	return (
+		<>
+			<ButtonGroup variant='contained' color='primary'>
+				<Button>
+					<Link to={firstLink}>{first}</Link>
+				</Button>
+				<Button>
+					<Link to={secondLink}>{second}</Link>
+				</Button>
+			</ButtonGroup>
+		</>
+	);
 };
 
 Buttons.propTypes = {
-  first: PropTypes.any.isRequired,
-  second: PropTypes.string.isRequired,
-  firstLink: PropTypes.string.isRequired,
-  secondLink: PropTypes.string.isRequired,
-}
+	first: PropTypes.any.isRequired,
+	second: PropTypes.string.isRequired,
+	firstLink: PropTypes.string.isRequired,
+	secondLink: PropTypes.string.isRequired,
+};
 
 export default Buttons;
